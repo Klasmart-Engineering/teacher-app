de738ef1627aa2ce0c8ed57bc64def12d7537f0b
Rihan Farman <rihan.farman@kidsloop.live>
2022-05-12 16:37:59 +0100

    Adding Crashlytics

--- android/app/src/main/java/net/kidsloop/teacher/MainActivity.java
+++ android/app/src/main/java/net/kidsloop/teacher/MainActivity.java
@@ -1,9 +1,9 @@
-package com.teacherapp;
+package net.kidsloop.teacher;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import net.kidsloop.teacher.BuildConfig;
 public class MainActivity extends ReactActivity {
 
   /**
